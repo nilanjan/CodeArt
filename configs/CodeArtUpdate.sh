71b87ca3cda68c1f31a1c0b38e8bd3a8217f8494
Nilanjan Goswami <512388+nilanjan@users.noreply.github.com>
2022-02-15 00:11:59 -0800

    Added more plugins and small script issue fix

--- configs/CodeArtUpdate.sh
+++ configs/CodeArtUpdate.sh
@@ -2,4 +2,4 @@
 
 mv ~/.config/nvim/lua/user_settings.lua ~/.config/
 svn export --force https://github.com/artart222/CodeArt.git/trunk/configs/ ~/.config/nvim
-mv --force ~/.config/user_settings.lua ~/.config/nvim/lua/user_settings.lua
+mv -f ~/.config/user_settings.lua ~/.config/nvim/lua/user_settings.lua
--- configs/lua/user_settings.lua
+++ configs/lua/user_settings.lua
@@ -5,13 +5,87 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+-- Load lua path
+local lua_path = function(name)
+  return string.format("require'plugins.%s'", name)
+end
+
 additional_plugins = {
   -- You can put your additional plugins here.
   -- Syntax is like normal packer.nvim Syntax.
   -- If you need to set some settings for your plugins
   -- you must put configs in config = function() like below examples
-  
-  -- { "famiu/feline.nvim", branch = "develop" },
+
+  { "famiu/feline.nvim", branch = "develop" },
+
+    -- LSP
+  { 'onsails/lspkind-nvim' },
+  { 'weilbith/nvim-code-action-menu', cmd = 'CodeActionMenu' },
+
+  -- Treesitter
+  { 'p00f/nvim-ts-rainbow', config = lua_path"nvim-ts-rainbow" },
+  { 'romgrk/nvim-treesitter-context' },
+  { 'SmiteshP/nvim-gps', config = lua_path"nvim-gps" },
+
+    -- Tmux
+  { 'christoomey/vim-tmux-navigator' },
+
+
+  -- Registers & clipboard
+  { 'tversteeg/registers.nvim' },
+  { 'AckslD/nvim-neoclip.lua', config = lua_path"nvim-neoclip" },
+
+  -- Move & Search & replace
+  { 'nacro90/numb.nvim', config = lua_path"numb" },
+  { 'dyng/ctrlsf.vim', config = lua_path"ctrlsf" },
+  { 'kevinhwang91/nvim-hlslens', config = lua_path"hlslens" },
+  { 'ggandor/lightspeed.nvim', config = lua_path"lightspeed" },
+  { 'ThePrimeagen/harpoon', config = lua_path"harpoon" },
+  { 'chaoren/vim-wordmotion' },
+  { 'fedepujol/move.nvim' },
+
+  -- Tags
+  { 'ludovicchabant/vim-gutentags', config = lua_path"vim-gutentags" },
+
+  -- DAP
+  { 'nvim-telescope/telescope-dap.nvim' },
+
+    -- Syntax
+  { 'moll/vim-node' },
+  { 'editorconfig/editorconfig-vim', config = lua_path"editorconfig" },
+  { 'chrisbra/csv.vim' },
+  { 'npxbr/glow.nvim' },
+  { 'junegunn/vim-easy-align' },
+  { 'mhartington/formatter.nvim', config = lua_path"formatter" },
+  { 'zdharma-continuum/zinit-vim-syntax' },
+  { 'rust-lang/rust.vim' },
+
+    -- Icons
+  { 'ryanoasis/vim-devicons' },
+
+  -- Color
+  { 'lpinilla/vim-codepainter' },
+
+  -- Git
+  { 'kdheepak/lazygit.nvim' },
+  { 'rhysd/committia.vim' },
+
+-- Telescope
+  { 'nvim-lua/popup.nvim' },
+  { 'nvim-telescope/telescope-project.nvim' },
+  { 'pwntester/octo.nvim', config = lua_path"octo" },
+
+  -- General Plugins
+  { 'rcarriga/nvim-notify', config = lua_path"nvim-notify" },
+  { 'airblade/vim-rooter', config = lua_path"vim-rooter" },
+  { 'mhinz/vim-startify' },
+  { 'goolord/alpha-nvim', config = lua_path"alpha-nvim" },
+  { 'jeffkreeftmeijer/vim-numbertoggle' },
+  { 'lambdalisue/suda.vim' },
+  { 'numtostr/FTerm.nvim', config = lua_path"fterm" },
+  { 'wfxr/minimap.vim', config = lua_path"minimap" },
+  { 'luukvbaal/stabilize.nvim', config = lua_path"stabilize" },
+  { "beauwilliams/focus.nvim", config = lua_path"focus" },
 
   -- "mhartington/formatter.nvim",
 
@@ -52,5 +126,7 @@ additional_plugins = {
 user_lualine_style = 1 -- You can choose between 1, 2, 3, 4 and 5
 user_indent_blankline_style = 1 -- You can choose between 1, 2, 3, 4,5 and 6
 
+vim.g.tokyonight_style = "strom"
+vim.cmd("colorscheme tokyonight")
 vim.cmd("autocmd FileType lua set tabstop=2")
 vim.cmd("autocmd Filetype lua set shiftwidth=2")
