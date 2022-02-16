43c555645fa8dcb65b0a2db104ed30998dfdd4d0
Nilanjan Goswami <512388+nilanjan@users.noreply.github.com>
2022-02-16 10:44:03 -0800

    Update codeart sync SH to point to my repo

--- configs/CodeArtUpdate.sh
+++ configs/CodeArtUpdate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 mv ~/.config/nvim/lua/user_settings.lua ~/.config/
-svn export --force https://github.com/artart222/CodeArt.git/trunk/configs/ ~/.config/nvim
+svn export --force https://github.com/nilanjan/CodeArt.git/trunk/configs/ ~/.config/nvim
 mv -f ~/.config/user_settings.lua ~/.config/nvim/lua/user_settings.lua
